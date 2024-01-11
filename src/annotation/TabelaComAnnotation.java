c7c0cc309c7096fe664b48d637cd57c8267b0877
Guilherme <turtwig102@gmail.com>
2024-01-11 14:08:23 -0300

    Tarefa feita

---
+++ src/annotation/TabelaComAnnotation.java
@@ -0,0 +1,9 @@
+package annotation;
+
+@TabelaAnnotation(value = "Minha Tabela")
+public class TabelaComAnnotation
+{
+    public static void main(String[] args) {
+        System.out.println("Tarefa Annotation");
+    }
+}
